7b9d9f213cb24b789d966e791802fff6cfc26992
Tiago Nascimento <tiago.nascimento@spruceid.com>
2021-03-01 21:27:10 -0300

    Add Custom Wallet Protocol on ServiceWorker
    Signed-off-by: Tiago Nascimento <tiago.nascimento@spruceid.com>

--- sw.js
+++ sw.js
@@ -1,17 +1,57 @@
-importScripts("flutter_service_worker.js");
-
-chrome.runtime.onMessageExternal.addListener((request, sender, senderResponse) => {
-  console.log(request, sender, senderResponse);
-})
-
 self.addEventListener("install", () => {
   chrome.windows.create({
     focused: true,
     url: chrome.runtime.getURL('index.html'),
-    width: 338,
-    height: 600,
+    width: 394,
+    height: 700,
     top: 64,
     left: 999999,
     type: "popup"
-  })
- });
+  });
+});
+
+const clients = new Map();
+const events = new Map();
+
+/// REQUESTER
+chrome.runtime.onMessageExternal.addListener((request, sender, callback) => {
+  chrome.windows.create({
+    focused: true,
+    url: chrome.runtime.getURL("index.html"),
+    width: 394,
+    height: 700,
+    top: 64,
+    left: 999999,
+    type: "popup",
+  }, (w) => {
+    clients[w.tabs[0].id] = callback;
+    events[w.tabs[0].id] = {
+      ...request,
+      origin: sender.origin || "unknown origin",
+    };
+  });
+});
+
+/// RESPONDER
+chrome.runtime.onConnect.addListener((port) => {
+  const tabId = port.sender.tab.id;
+
+  const timeout = setTimeout(() => port.disconnect(), 30000);
+
+  port.onDisconnect.addListener((_) => {
+    clearTimeout(timeout);
+    clients[tabId](null);
+    clients.delete(tabId);
+    port.disconnect();
+  });
+
+  port.onMessage.addListener((e) => {
+    clearTimeout(timeout);
+    clients[tabId](e);
+    clients.delete(tabId);
+    port.disconnect();
+  });
+
+  port.postMessage(events[tabId]);
+  events.delete(tabId);
+});
